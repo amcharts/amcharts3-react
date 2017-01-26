73ad4483a2a2ab7dd899a0fb226d2c1681d5d0c8
Pauan <pcxunlimited@gmail.com>
2017-01-26 00:47:33 -1000

    Fixing a bug which caused the stock charts to not update

--- amcharts3-react.js
+++ amcharts3-react.js
@@ -4,13 +4,19 @@
     return {}.toString.call(x);
   }
 
+  function hasOwnKey(obj, key) {
+    return {}.hasOwnProperty.call(obj, key);
+  }
+
 
   function copyObject(x) {
     var output = {};
 
     // TODO use Object.keys ?
     for (var key in x) {
-      output[key] = copy(x[key]);
+      if (hasOwnKey(x, key)) {
+        output[key] = copy(x[key]);
+      }
     }
 
     return output;
@@ -48,119 +54,181 @@
   }
 
 
-  function isEqualArray(x, y) {
-    var xLength = x.length;
-    var yLength = y.length;
-
-    if (xLength === yLength) {
-      for (var i = 0; i < xLength; ++i) {
-        if (!isEqual(x[i], y[i])) {
-          return false;
-        }
-      }
-
-      return true;
-
-    } else {
-      return false;
-    }
-  }
-
-  function isEqualObject(x, y) {
-    // TODO use Object.keys ?
-    for (var key in x) {
-      if (key in y) {
-        if (!isEqual(x[key], y[key])) {
-          return false;
-        }
-
-      } else {
-        return false;
-      }
-    }
-
-    // TODO use Object.keys ?
-    for (var key in y) {
-      if (!(key in x)) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
   function isNaN(x) {
-    return x !== x
+    return x !== x;
   }
 
-  function isEqual(x, y) {
-    var xType = getType(x);
-    var yType = getType(y);
+  function isNumberEqual(x, y) {
+    return x === y || (isNaN(x) && isNaN(y));
+  }
 
-    if (xType === yType) {
-      switch (xType) {
-      case "[object Array]":
-        return isEqualArray(x, y);
 
-      case "[object Object]":
-        return isEqualObject(x, y);
-
-      case "[object Date]":
-        return x.getTime() === y.getTime();
-
-      case "[object Number]":
-        return x === y || (isNaN(x) && isNaN(y))
-
-      default:
-        return x === y;
+  function removeChartListeners(chart, x, y) {
+    if (x !== y) {
+      // TODO is this necessary ?
+      if (x == null) {
+        x = [];
       }
 
-    } else {
-      return false;
-    }
-  }
-
-
-  function removeChartListeners(chart, listeners) {
-    if (listeners != null) {
-      // TODO use Object.keys ?
-      for (var key in listeners) {
-        var listener = listeners[key];
-
-        chart.removeListener(chart, listener.event, listener.method);
+      // TODO is this necessary ?
+      if (y == null) {
+        y = [];
       }
-    }
-  }
 
-  // TODO make this faster ?
-  // TODO does this work for listeners, etc. ?
-  function updateChartObject(chart, oldObj, newObj) {
-    var didUpdate = false;
+      var xLength = x.length;
+      var yLength = y.length;
 
-    // TODO use Object.keys ?
-    for (var key in newObj) {
-      // TODO make this faster ?
-      if (!(key in oldObj) || !isEqual(oldObj[key], newObj[key])) {
-        if (key === "listeners") {
-          // TODO make this faster ?
-          removeChartListeners(chart, oldObj[key]);
-        }
+      for (var i = 0; i < xLength; ++i) {
+        var xValue = x[i];
+
+        var has = false;
 
         // TODO make this faster ?
-        chart[key] = copy(newObj[key]);
+        for (var j = 0; j < yLength; ++j) {
+          var yValue = y[j];
+
+          // TODO is this correct ?
+          if (xValue.event  === yValue.event &&
+              xValue.method === yValue.method) {
+            has = true;
+            break;
+          }
+        }
+
+        if (!has) {
+          // TODO is this correct ?
+          chart.removeListener(chart, xValue.event, xValue.method);
+        }
+      }
+    }
+  }
+
+
+  function updateArray(a, x, y) {
+    var didUpdate = false;
+
+    if (x !== y) {
+      var xLength = x.length;
+      var yLength = y.length;
+
+      if (xLength !== yLength) {
+        a.length = yLength;
+        didUpdate = true;
+      }
+
+      for (var i = 0; i < yLength; ++i) {
+        if (i < xLength) {
+          if (update(a, i, x[i], y[i])) {
+            didUpdate = true;
+          }
+
+        } else {
+          // TODO make this faster ?
+          a[i] = copy(y[i]);
+          // TODO is this necessary ?
+          didUpdate = true;
+        }
+      }
+    }
+
+    return didUpdate;
+  }
+
+
+  function update(obj, key, x, y) {
+    var didUpdate = false;
+
+    if (x !== y) {
+      var xType = getType(x);
+      var yType = getType(y);
+
+      if (xType === yType) {
+        switch (xType) {
+        case "[object Array]":
+          if (updateArray(obj[key], x, y)) {
+            didUpdate = true;
+          }
+          break;
+
+        case "[object Object]":
+          if (updateObject(obj[key], x, y)) {
+            didUpdate = true;
+          }
+          break;
+
+        case "[object Date]":
+          if (x.getTime() !== y.getTime()) {
+            // TODO make this faster ?
+            obj[key] = copy(y);
+            didUpdate = true;
+          }
+          break;
+
+        case "[object Number]":
+          if (!isNumberEqual(x, y)) {
+            // TODO is the copy necessary ?
+            obj[key] = copy(y);
+            didUpdate = true;
+          }
+          break;
+
+        default:
+          if (x !== y) {
+            // TODO is the copy necessary ?
+            obj[key] = copy(y);
+            didUpdate = true;
+          }
+          break;
+        }
+
+      // TODO is this correct ?
+      } else {
+        // TODO make this faster ?
+        obj[key] = copy(y);
         didUpdate = true;
       }
     }
 
-    // TODO use Object.keys ?
-    for (var key in oldObj) {
-      if (!(key in newObj)) {
-        if (key === "listeners") {
-          removeChartListeners(chart, oldObj[key]);
-        }
+    return didUpdate;
+  }
 
-        delete chart[key];
-        didUpdate = true;
+  function updateObject(chart, oldObj, newObj) {
+    var didUpdate = false;
+
+    if (oldObj !== newObj) {
+      // TODO use Object.keys ?
+      for (var key in newObj) {
+        if (hasOwnKey(newObj, key)) {
+          // TODO make this faster ?
+          if (hasOwnKey(oldObj, key)) {
+            // TODO should this count as an update ?
+            if (key === "listeners") {
+              // TODO make this faster ?
+              removeChartListeners(chart, oldObj[key], newObj[key]);
+            }
+
+            if (update(chart, key, oldObj[key], newObj[key])) {
+              didUpdate = true;
+            }
+
+          } else {
+            // TODO make this faster ?
+            chart[key] = copy(newObj[key]);
+            didUpdate = true;
+          }
+        }
+      }
+
+      // TODO use Object.keys ?
+      for (var key in oldObj) {
+        if (hasOwnKey(oldObj, key) && !hasOwnKey(newObj, key)) {
+          if (key === "listeners") {
+            removeChartListeners(chart, oldObj[key], []);
+          }
+
+          delete chart[key];
+          didUpdate = true;
+        }
       }
     }
 
@@ -189,11 +257,14 @@
 
     // TODO is this correct ? should this use componentWillUpdate instead ?
     componentDidUpdate: function (oldProps) {
-      var didUpdate = updateChartObject(this.state.chart, oldProps, this.props);
+      var didUpdate = updateObject(this.state.chart, oldProps, this.props);
 
       if (didUpdate) {
         // TODO is this correct ?
-        this.state.chart.validateNow(true, false);
+        if (this.state.chart.chartCreated && this.state.chart.chartRendered) {
+          // TODO is this correct ?
+          this.state.chart.validateNow(true, false);
+        }
       }
     },
 
